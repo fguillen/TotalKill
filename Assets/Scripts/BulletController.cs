c5da7d9fcdf4edeb84f12c32653b2e389289c4ea
Fernando Guillen <fguillen.mail@gmail.com>
2017-08-20 23:29:22 +0200

    Cleaning up the BulletController

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -5,36 +5,9 @@ using UnityEngine.UI;
 
 public class BulletController : MonoBehaviour {
 
-	private Text text;
 	public GameObject bulletExplosion;
 
-	// Use this for initialization
-	void Start () {
-		Debug.Log ("BulletController.Start");
-		text = GameObject.Find ("Text").GetComponent<Text>(); 
-		text.text = "Changed!";
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-//	void OnTriggerEnter2D(Collider2D hit){
-//
-//		text.text = "Here!";
-//		
-//		Debug.Log ("bullet collision 1: ");
-//		Debug.Log ("bullet collision: " + hit);
-//
-//		Instantiate (bulletExplosion, transform.position, Quaternion.identity);
-//		Destroy (gameObject);
-//	}
-//
 	void OnCollisionEnter2D(Collision2D collision){
-
-		text.text = "Here Collision!";
-
 		Debug.Log ("bullet collision 1: ");
 		Debug.Log ("bullet collision: " + collision);
 
@@ -42,8 +15,6 @@ public class BulletController : MonoBehaviour {
 	
 		Destroy (gameObject);
 		Destroy (bulletExplosionCloned, 2.0f);
-
-
 	}
 
 }
