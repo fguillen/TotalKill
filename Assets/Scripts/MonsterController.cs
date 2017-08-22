dc2b5853136a4e9858dd4376e1e225df7eee446a
Fernando Guillen <fguillen.mail@gmail.com>
2017-08-23 00:07:26 +0200

    Monster Animations

--- Assets/Scripts/MonsterController.cs
+++ Assets/Scripts/MonsterController.cs
@@ -9,10 +9,11 @@ public class MonsterController : MonoBehaviour {
 	public int energy;
 	private Rigidbody2D rigidbody2D;
 	public Text monsterEnergyText;
-
+	private Animator animator;
 
 	// Use this for initialization
 	void Start () {
+		animator = GetComponent<Animator> ();
 		rigidbody2D = GetComponent<Rigidbody2D> ();
 		ChangeDirection ();
 		UpdateMonsterEnergyText ();
@@ -24,11 +25,42 @@ public class MonsterController : MonoBehaviour {
 	}
 
 	void ChangeDirection() {
-		float moveX = Random.Range (-1f, 1f);
-		float moveY = Random.Range (-1f, 1f);
+		int direction = Random.Range (0, 5);
+
+		float moveX = 0;
+		float moveY = 0;
+
+		if (direction == 1) {
+			moveX = 0;
+			moveY = -1;
+		}
+
+		if (direction == 2) {
+			moveX = 1;
+			moveY = 0;
+		}
+
+		if (direction == 3) {
+			moveX = 0;
+			moveY = 1;
+		}
+
+		if (direction == 4) {
+			moveX = -1;
+			moveY = 0;
+		}
 
 		float velocityX = moveX * speed;
 		float velocityY = moveY * speed;
+
+		animator.SetFloat ("VelocityX", velocityX);
+		animator.SetFloat ("VelocityY", velocityY);
+
+		if (Mathf.Abs(velocityX) > 0 || Mathf.Abs(velocityY) > 0 ) {
+			animator.SetBool ("Walking", true);
+		} else {
+			animator.SetBool ("Walking", false);
+		}
 
 		Vector2 newVelocity	= new Vector2 (velocityX, velocityY);
 
